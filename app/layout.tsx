c24a43477b61fcc459589a6675ab0af1948b1285
seydinath <seydinadiagne2@outlook.com>
2025-08-28 13:39:44 +0000

    changmt balise

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+
 import "./globals.css";
 import { Suspense } from "react";
+import Script from "next/script";
 
 export const metadata = {
   title: "Seydina Th.Diagne - Software Engineer & CCNA Certified",
@@ -14,36 +16,25 @@ function Footer() {
   );
 }
 
-import Script from "next/script";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fr">
-      <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NFT8SKWB');`}
-        </Script>
-      </head>
-      <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NFT8SKWB"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        <div>
-          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
-          <Footer />
-        </div>
-      </body>
-    </html>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-WH7E4SF2R9"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-WH7E4SF2R9');
+        `}
+      </Script>
+      <div>
+        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+        <Footer />
+      </div>
+    </>
   );
 }
