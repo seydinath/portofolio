eb5ffbb1ffc63221e5e045b073a1cc7373390c3b
seydinath <seydinadiagne2@outlook.com>
2025-08-28 12:38:37 +0000

    ajout balises g.analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,36 @@ function Footer() {
   );
 }
 
+import Script from "next/script";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
-      <Footer />
-    </div>
+    <html lang="fr">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NFT8SKWB');`}
+        </Script>
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NFT8SKWB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        <div>
+          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+          <Footer />
+        </div>
+      </body>
+    </html>
   );
 }
