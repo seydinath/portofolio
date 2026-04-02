b12ce2dc05632d2bd1254b0ce93657312a391a6f
seydinath <seydinadiagne2@outlook.com>
2026-04-02 21:00:04 +0000

    MajIcons

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/icon-dark-32x32.png',
         media: '(prefers-color-scheme: light)',
       },
       {
